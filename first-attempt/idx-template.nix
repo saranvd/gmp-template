46c3e3e96781d66af046e2f90eef36600abae9ab
snavidpour <snavidpour@google.com>
2024-04-02 22:08:14 +0000

    Updated the nix file

--- first-attempt/idx-template.nix
+++ first-attempt/idx-template.nix
@@ -4,10 +4,9 @@
     ];
   bootstrap = ''
     mkdir -p "$WS_NAME"
-    mkdir -p "$WS_NAME/.idx/"
+    git clone ${giturl}
     echo ${apikey} > "$WS_NAME/apikey.txt"
     chmod -R +w "$WS_NAME"
     mv "$WS_NAME" "$out"
-    git clone ${giturl}
   '';
 }
