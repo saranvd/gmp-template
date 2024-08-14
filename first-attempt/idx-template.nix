542991537dba869e371b894fab25009bc32655c8
snavidpour <snavidpour@google.com>
2024-08-14 19:55:07 +0000

    debugging on secret.properties file

--- first-attempt/idx-template.nix
+++ first-attempt/idx-template.nix
@@ -14,13 +14,13 @@
     # Find every local.defaults.properties file in the repo ane replace the MAPS_API_KEY property with said value
     # find $WS_NAME -type f -name 'local.defaults.properties' -exec sed -i "s/\(MAPS_API_KEY=\).*/\1\"DEFAULT_API_KEY\"/" {} \;
 
-    # Create a secrets.properties file in the repo ane replace the MAPS_API_KEY property with said value
-    touch secrets.properties
-    echo "MAPS_API_KEY=DEFAULT_API_KEY" > secrets.properties
-    find $WS_NAME -type f -name 'secrets.properties' -exec sed -i "s/\(MAPS_API_KEY=\).*/\1\"${apikey}\"/" {} \;
-
     chmod -R +w "$WS_NAME"
     mkdir -p "$WS_NAME/.idx/"
+
+    # Create a secrets.properties file in the repo ane replace the MAPS_API_KEY property with said value
+    touch $WS_NAME/secrets.properties
+    echo "MAPS_API_KEY=DEFAULT_API_KEY" > $WS_NAME/secrets.properties
+    find $WS_NAME -type f -name 'secrets.properties' -exec sed -i "s/\(MAPS_API_KEY=\).*/\1\"${apikey}\"/" {} \;
 
     # We create a dev.nix that builds the subproject specified at template instantiation
     launch_activity=${launchactivity} j2 --format=env ${./devNix.j2} -o $WS_NAME/.idx/dev.nix
