4197ea05fbfb97ca44e98ed5088e99b4f3ce2443
snavidpour <snavidpour@google.com>
2024-08-14 18:54:41 +0000

    Creating secrets.properties file instead of using local.defaults.properties

--- first-attempt/idx-template.nix
+++ first-attempt/idx-template.nix
@@ -12,7 +12,11 @@
     mv tmp/${subdir}/* "$WS_NAME"
 
     # Find every local.defaults.properties file in the repo ane replace the MAPS_API_KEY property with said value
-    find $WS_NAME -type f -name 'local.defaults.properties' -exec sed -i "s/\(MAPS_API_KEY=\).*/\1\"${apikey}\"/" {} \;
+    find $WS_NAME -type f -name 'local.defaults.properties' -exec sed -i "s/\(MAPS_API_KEY=\).*/\1\"DEFAULT_API_KEY\"/" {} \;
+
+    # Create a secrets.properties file in the repo ane replace the MAPS_API_KEY property with said value
+    touch secrets.properties
+    echo "MAPS_API_KEY=${apikey}" > secrets.properties
 
     chmod -R +w "$WS_NAME"
     mkdir -p "$WS_NAME/.idx/"
