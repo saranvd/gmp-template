8ca1006f57ab1dd3ea7cd3e6f0a651aa85ef2ca5
snavidpour <snavidpour@google.com>
2024-08-14 17:59:46 +0000

    No need to update the gradle version any more

--- first-attempt/idx-template.nix
+++ first-attempt/idx-template.nix
@@ -14,9 +14,6 @@
     # Find every local.defaults.properties file in the repo ane replace the MAPS_API_KEY property with said value
     find $WS_NAME -type f -name 'local.defaults.properties' -exec sed -i "s/\(MAPS_API_KEY=\).*/\1\"${apikey}\"/" {} \;
 
-    # The maps samples seem to have really outdated versions of gradle. We simple search for all gradle properties files and upgrade the gradle distribution
-    find $WS_NAME -type f -name 'gradle-wrapper.properties' -exec sed -i 's|distributionUrl=.*|distributionUrl=https\\://services.gradle.org/distributions/gradle-8.7-bin.zip|' {} \;
-
     chmod -R +w "$WS_NAME"
     mkdir -p "$WS_NAME/.idx/"
 
